0b1b0bd30ab015c0ae294d28017fd983b3d713c6
noita <o.a.raty@gmail.com>
2013-03-07 13:40:07 +0200

    projekti lisatty

---
+++ OhjHarjoitus/src/ohjharjoitus/OhjHarjoitus.java
@@ -0,0 +1,16 @@
+
+package ohjharjoitus;
+
+/**
+ *
+ * @author O
+ */
+public class OhjHarjoitus {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
